472184501280f662c608646aec7a772159e575d1
AliAbdallah11 <alininjaabdallah57@gmail.com>
2025-06-23 17:43:08 +0300

    Consistent

--- Dictionaries and lists.py
+++ Dictionaries and lists.py
@@ -16,3 +16,6 @@ max_average = max(average_grades(grades) for grades in class_journal.values())
 top_students = [student for student, grades in class_journal.items() if average_grades(grades) == max_average]
 print(f"The highest average grade in the class is: {top_students}: {round(max_average, 2)}")
 
+for student, grades in class_journal.items():
+    most_consistent_student = min(class_journal, key=lambda student: max(class_journal[student]) - min(class_journal[student]))
+print(f"The most consistent student is: {most_consistent_student} with grades: {class_journal[most_consistent_student]}")
